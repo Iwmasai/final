10f4a18c3249bca060ed2f376f371bd9823181da
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-12 12:30:07 +0900

    Update menu.php

--- src/menu.php
+++ src/menu.php
@@ -1,13 +1,31 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517350-final';
+    const USER = 'LAA1517350';
+    const PASS = 'Pass0902';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+    ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    echo '<h1>ラーメン店一覧</h1>';
-    ?>
+<h1>ラーメン店一覧</h1>
+<table>
+    <tr><th>店名</th><th>住所</th><th>カテゴリー</th>
+<?php
+    $pdo=new PDO($connect, USER,PASS);
+    foreach($pdo->query('select * from ra-men')as $row) {
+        echo '<tr>';
+        echo '<td>',$row['name'],'</td>';
+        echo '<td>',$row['address'],'</td>';
+        echo '</tr>';
+        echo "\n";
+        }
+        ?>
+        </table>
 </body>
 </html>
