6e9177ff9b9eda65880cb611d9560abef84c4c42
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-15 14:53:57 +0900

    Update ra-men-output.php

--- src/ra-men-output.php
+++ src/ra-men-output.php
@@ -4,7 +4,8 @@ error_reporting(E_ALL);
 require 'db-connect.php';
 
 $pdo = new PDO($connect, USER, PASS);
-$sql = $pdo->prepare('INSERT INTO ra-men (name, address, category_id) VALUES (?, ?, ?)');
+$sql = $pdo->prepare('INSERT INTO `ra-men` (name, address, category_id) VALUES (?, ?, ?)');
+
 
 if (empty($_POST['name'])) {
     echo '店舗名を入力してください。';
