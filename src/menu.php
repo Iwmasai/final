e0a53b9d29ecd29c44171119c414b28740790e0d
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-15 10:11:21 +0900

    Update menu.php

--- src/menu.php
+++ src/menu.php
@@ -1,38 +1,43 @@
 <?php
-    const SERVER = 'mysql220.phy.lolipop.lan';
-    const DBNAME = 'LAA1517350-final';
-    const USER = 'LAA1517350';
-    const PASS = 'Pass0902';
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517350-final';
+const USER = 'LAA1517350';
+const PASS = 'Pass0902';
 
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-    ?>
+$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+
+$pdo = new PDO($connect, USER, PASS);
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>ラーメン店一覧</title>
 </head>
 <body>
-<h1>ラーメン店一覧</h1>
-<table>
-    <tr><th>店名</th><th>住所</th>
-<?php
-    $pdo=new PDO($connect, USER,PASS);
-    foreach($pdo->query('select * from ra_men')as $row) {
-        echo '<tr>';
-        echo '<td>',$row['name'],'</td>';
-        echo '<td>',$row['address'],'</td>';
-        echo '</tr>';
-        echo "\n";
-        }
+    <h1>ラーメン店一覧</h1>
+    <table>
+        <tr>
+            <th>店名</th>
+            <th>カテゴリー</th>
+            <th>住所</th>
+        </tr>
 
-        foreach ($pdo->query('SELECT * FROM category') as $row) {
+        <?php
+        $query = 'SELECT ra_men.name, category.category_name, ra_men.address
+                  FROM ra_men
+                  LEFT JOIN category ON ra_men.category_id = category.category_id';
+
+        foreach ($pdo->query($query) as $row) {
             echo '<tr>';
             echo '<td>', $row['name'], '</td>';
+            echo '<td>', $row['category_name'], '</td>';
+            echo '<td>', $row['address'], '</td>';
             echo '</tr>';
             echo "\n";
         }
         ?>
-        </table>
+    </table>
 </body>
 </html>
