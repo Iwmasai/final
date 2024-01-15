dfc71cf853245ab0b501c07df2d1518ca77aaa03
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-15 14:45:03 +0900

    Update ra-men-output.php

--- src/ra-men-output.php
+++ src/ra-men-output.php
@@ -1,35 +1,27 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-require 'db-connect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>練習6-5</title>
-</head>
-<body>
-    <?php
-    $pdo = new PDO($connect, USER, PASS);
-    $sql = $pdo->prepare('INSERT INTO ra-men (name, address) VALUES (?, ?)');
+require 'db-connect.php';
 
-    
-    if (empty($_POST['name'])) {
-        echo '店舗名を入力してください。';
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('INSERT INTO ra-men (name, address, category_id) VALUES (?, ?, ?)');
+
+if (empty($_POST['name'])) {
+    echo '店舗名を入力してください。';
+} else {
+    $category_id = $_POST['category'];
+
+    if ($sql->execute([$_POST['name'], $_POST['address'], $category_id])) {
+        echo '<font color="red">追加に成功しました。</font>';
+        // 登録が成功したら menu.php にリダイレクト
+        header('Location: menu.php');
+        exit;
     } else {
-        // ここで category_id を指定
-        $category_id = $_POST['category'];
-
-        if ($sql->execute([$_POST['name'], $_POST['address'], $category_id])) {
-            echo '<font color="red">追加に成功しました。</font>';
-        } else {
-            echo '<font color="red">追加に失敗しました。</font>';
-        }
+        echo '<font color="red">追加に失敗しました。</font>';
     }
-    ?>
-    
-    <form action="ra-men-input.php" method="post">
-        <button type="submit">追加画面に戻る</button>
-    </form>
-</body>
-</html>
+}
+?>
+
+<form action="ra-men-input.php" method="post">
+    <button type="submit">追加画面に戻る</button>
+</form>
