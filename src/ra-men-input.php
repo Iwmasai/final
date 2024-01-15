280c94fa0dcf5f390185221edd0acd8b495bcfce
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-15 10:54:17 +0900

    Update ra-men-input.php

--- src/ra-men-input.php
+++ src/ra-men-input.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <mate charset="UTF-8">
-        <title>ラーメン店登録</title>
-    </head>
-    <body>
-        <p>ラーメン店を追加します。</p>
-        <form action="ren6-5-output.php" method="post">
-            商品名<input type="text" name="name"><br>
-            カテゴリー
-            住所<input type="text" name="address"><br>
-            <input type="submit" value="追加">
-            </form>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>ラーメン店登録</title>
+</head>
+<body>
+    <p>ラーメン店を追加します。</p>
+    <form action="ra-men-output.php" method="post">
+        店舗名: <input type="text" name="name"><br>
+        カテゴリー: 
+        <select name="category">
+            <option value="1">とんこつ</option>
+            <option value="2">魚介とんこつ</option>
+            <option value="3">塩</option>
+            <option value="4">醤油</option>
+            <option value="5">味噌</option>
+        </select><br>
+        住所: <input type="text" name="address"><br>
+        <input type="submit" value="追加">
+    </form>
+</body>
 </html>
