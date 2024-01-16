73ff3d69436d3ea2b2a2a042b4018d7660d0580a
Iwmasai <2201410@s.asojuku.ac.jp>
2024-01-16 14:21:14 +0900

    Update delete.php

--- src/delete.php
+++ src/delete.php
@@ -2,6 +2,20 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require 'db-connect.php';
+
+ob_start(); // バッファリング開始
+
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('DELETE FROM ra_men WHERE id = ?');
+if ($sql->execute([$_POST['id']])) {
+    header('Location: menu.php');
+    exit;
+} else {
+    echo '削除に失敗しました。';
+}
+
+ob_end_flush(); // バッファリング終了
+?>
 <!DOCTYPE html>
 <html lang='ja'>
 <head>
@@ -9,15 +23,6 @@ require 'db-connect.php';
     <title>delete.php</title>
 </head>
 <body>
-    $pdo = new PDO($connect, USER, PASS);
-    $sql = $pdo->prepare('DELETE FROM ra_men WHERE id = ?');
-    if ($sql->execute([$_POST['id']])) {
-        header('Location: menu.php');
-        exit;
-    } else {
-        echo '削除に失敗しました。';
-    }
-?>
     <button onclick="location.href='menu.php'">トップへ戻る</button>
 </body>
 </html>
